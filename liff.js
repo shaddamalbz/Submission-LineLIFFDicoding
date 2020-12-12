27d4b8fc8b83141646458fdf810f068e6878ff9b
Shaddam Alghafiqih <shaddamalghafiqih11@gmail.com>
2020-12-12 21:06:28 +0700

    first commit

--- liff.js
+++ liff.js
@@ -1,7 +1,6 @@
 window.onload = function() {
   const useNodeJS = false;   // if you are not using a node server, set this value to false
   const defaultLiffId = "1655317045-Drda4KgG";   // change the default LIFF value if you are not using a node server
-
   // DO NOT CHANGE THIS
   let myLiffId = "";
 
